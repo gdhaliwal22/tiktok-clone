0999017358adffd6e0ea9607fa13a8106bdeb536
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-06 13:03:25 -0700

    created VideoSidebar component and css

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "../css/Video.css";
 
 function Video() {
   return (
     <div className="video">
-      <video src="https://v16m.tiktokcdn.com/6ac76a66d80bc101bfe5e2bdd99a7f6b/5f559229/video/tos/useast2a/tos-useast2a-ve-0068c004/09d654183c3647549344148c1a473d0e/?a=1233&br=4802&bt=2401&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090619512001018907107042113406&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2x1NmVldnN4djMzaTczM0ApZGRpOzUzN2Q3Nzs3Mzc8OmdrNWAvNGlxZ2JfLS02MTZzc2MxLTUyMC4tMDM2YzNhMi06Yw%3D%3D&vl=&vr="></video>
+      <video
+        className="video__player"
+        loop
+        src="https://v16m.tiktokcdn.com/6ac76a66d80bc101bfe5e2bdd99a7f6b/5f559229/video/tos/useast2a/tos-useast2a-ve-0068c004/09d654183c3647549344148c1a473d0e/?a=1233&br=4802&bt=2401&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090619512001018907107042113406&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2x1NmVldnN4djMzaTczM0ApZGRpOzUzN2Q3Nzs3Mzc8OmdrNWAvNGlxZ2JfLS02MTZzc2MxLTUyMC4tMDM2YzNhMi06Yw%3D%3D&vl=&vr="
+      ></video>
     </div>
   );
 }
