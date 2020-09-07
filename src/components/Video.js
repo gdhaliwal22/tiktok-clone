98568e8a3b98ea3e00576cabc34d0d2a2a8cf85a
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-07 12:09:12 -0700

    add share favorite and message icons to sidebar

--- src/components/Video.js
+++ src/components/Video.js
@@ -29,6 +29,7 @@ function Video() {
         src="https://v77.tiktokcdn.com/6490b3ea84789a1a7ba7079c63aadc57/5f56c3ba/video/tos/useast2a/tos-useast2a-ve-0068c004/2362532506c24a66be8a02a69273732d/?a=1233&br=2652&bt=1326&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200907173510010190192131040E02D0&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2tmOHM1Ojc6dTMzMzczM0ApZDpnZmVmM2QzNzM3OmVmaWdeYm1nLjE0bWlfLS1iMTZzczIuNTZeYDNgNTM2XmFhMDU6Yw%3D%3D&vl=&vr="
       ></video>
       <VideoFooter />
+      <VideoSidebar />
     </div>
   );
 }
--- src/components/VideoSidebar.js
+++ src/components/VideoSidebar.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "../css/VideoSidebar.css";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MessageIcon from "@material-ui/icons/Message";
+import ShareIcon from "@material-ui/icons/Share";
+
 function VideoSidebar() {
-  return <div classNam="videoSidebar"></div>;
+  return (
+    <div classNam="videoSidebar">
+      <div className="videoSidebar__button">
+        <FavoriteIcon fontSize="large" />
+      </div>
+      <div className="videoSidebar__button">
+        <MessageIcon fontSize="large" />
+      </div>
+      <div className="videoSidebar__button">
+        <ShareIcon fontSize="large" />
+      </div>
+    </div>
+  );
 }
 
 export default VideoSidebar;
