992a7fac6be6d77351c260b1c60f635fe0d14e67
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-08 14:22:26 -0700

    refactored app to pull videos from database

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Video from "./components/Video";
+import axios from "./axios";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    async function fetchPosts() {
+      const res = await axios.get("/v2/posts");
+      setVideos(res.data);
+      return res;
+    }
+    fetchPosts();
+  }, []);
   return (
     <div className="app">
       <div className="app__videos">
-        <Video
-          url="https://v16m.tiktokcdn.com/4db8d77ebe69e646b313214853bdf21f/5f580eda/video/tos/useast2a/tos-useast2a-ve-0068/232de9c94cfe4a3c8218e6321c7eb8c6/?a=1233&br=3258&bt=1629&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090817080201018919521654158F34&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajdzOjw5aHdrcDMzZjczM0ApO2U0NDpoN2UzNzlmZWU8aGdwYWNlLm9yMjFfLS00MTZzczQzXjU2LzE1Yi42NGAvXmM6Yw%3D%3D&vl=&vr="
-          channel="gdhaliwal22"
-          description="Dogs and their puppy"
-          song="Dogs"
-          likes={111}
-          messages={235}
-          shares={28}
-        />
-        <Video
-          url="https://v16m.tiktokcdn.com/4db8d77ebe69e646b313214853bdf21f/5f580eda/video/tos/useast2a/tos-useast2a-ve-0068/232de9c94cfe4a3c8218e6321c7eb8c6/?a=1233&br=3258&bt=1629&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090817080201018919521654158F34&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajdzOjw5aHdrcDMzZjczM0ApO2U0NDpoN2UzNzlmZWU8aGdwYWNlLm9yMjFfLS00MTZzczQzXjU2LzE1Yi42NGAvXmM6Yw%3D%3D&vl=&vr="
-          channel="gdhaliwal22"
-          description="Dogs and their puppy"
-          song="Dogs"
-          likes={111}
-          messages={235}
-          shares={28}
-        />
+        {videos.map(
+          ({ url, channel, description, song, likes, messages, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              song={song}
+              likes={likes}
+              messages={messages}
+              description={description}
+              shares={shares}
+            />
+          )
+        )}
       </div>
-
-      {/* app container */}
-      {/* videos */}
     </div>
   );
 }
