aac3e93a2787ec84f2915d40d426170d6a8bf2d5
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-08 10:21:53 -0700

    pass channel description and song to VideoFooter

--- src/components/Video.js
+++ src/components/Video.js
@@ -28,8 +28,12 @@ function Video({ url, channel, description, song, likes, messages, shares }) {
         ref={videoRef}
         src="https://v16m.tiktokcdn.com/4db8d77ebe69e646b313214853bdf21f/5f580eda/video/tos/useast2a/tos-useast2a-ve-0068/232de9c94cfe4a3c8218e6321c7eb8c6/?a=1233&br=3258&bt=1629&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090817080201018919521654158F34&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajdzOjw5aHdrcDMzZjczM0ApO2U0NDpoN2UzNzlmZWU8aGdwYWNlLm9yMjFfLS00MTZzczQzXjU2LzE1Yi42NGAvXmM6Yw%3D%3D&vl=&vr="
       ></video>
-      <VideoFooter />
-      <VideoSidebar likes={likes} messages={messages} shares={shares} />
+      <VideoFooter
+        channel="gdhaliwal22"
+        description="Dogs and puppy"
+        song="Dogs"
+      />
+      <VideoSidebar likes={111} messages={200} shares={19} />
     </div>
   );
 }
