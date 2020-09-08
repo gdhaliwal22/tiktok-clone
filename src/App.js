d64f0a25839b1181677fc0a43663dd8fe3288c60
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-08 10:33:12 -0700

    props being passed down from App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ function App() {
   return (
     <div className="app">
       <div className="app__videos">
-        <Video />
+        <Video
+          url="https://v16m.tiktokcdn.com/4db8d77ebe69e646b313214853bdf21f/5f580eda/video/tos/useast2a/tos-useast2a-ve-0068/232de9c94cfe4a3c8218e6321c7eb8c6/?a=1233&br=3258&bt=1629&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090817080201018919521654158F34&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajdzOjw5aHdrcDMzZjczM0ApO2U0NDpoN2UzNzlmZWU8aGdwYWNlLm9yMjFfLS00MTZzczQzXjU2LzE1Yi42NGAvXmM6Yw%3D%3D&vl=&vr="
+          channel="gdhaliwal22"
+          description="Dogs and their puppy"
+          song="Dogs"
+          likes={111}
+          messages={235}
+          shares={28}
+        />
         <Video />
       </div>
 
